6eaf84c0c8c1c7976db7b90e3cf463023cc7b1df
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-12 15:37:11 +0000

    updated to latest version of the bitbucket version

--- src/components/sidebar/skillLevel.jsx
+++ src/components/sidebar/skillLevel.jsx
@@ -5,47 +5,49 @@ import { Link } from "react-router-dom";
 class SkillLevel extends Component {
 
 	constructor(props) {
-			super(props);
-			this.state = {
-				selectedSkill: ""
-			};
+		super(props);
+		this.state = {
+			selectedSkill: ""
+		};
 
-			this.handleChange 			= this.handleChange.bind(this);
-			this.redirectToSkillLevel 	= this.redirectToSkillLevel.bind(this);
-			this.SkillLevelFunction 	= this.SkillLevelFunction.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.redirectToSkillLevel = this.redirectToSkillLevel.bind(this);
+		this.SkillLevelFunction = this.SkillLevelFunction.bind(this);
 	}
 
 	handleChange = async (event) => {
-		 await this.setState({selectedSkill: event.target.value});
-		 return await this.redirectToSkillLevel();
+		await this.setState({ selectedSkill: event.target.value });
+		console.log("SELECTED SKILL");
+		console.log(this.state.selectedSkill);
+		return await this.redirectToSkillLevel();
 	}
 
 	redirectToSkillLevel = () => {
-		window.location = `${this.state.selectedSkill}`
+		window.location = `/opportunities/skilllevel/${this.state.selectedSkill}`
 	}
 
 	SkillLevelFunction = () => {
 
 		return (
 			<>
-			<div className="wmca-form wdgt">
-          		<label for="dropdown" class="wmca-form-label">Skill level</label>
-          		<select className="" onChange={this.handleChange}>
-					<option value="">Select</option>
-					<option value="advanced">Advanced</option>
-					<option value="intermediate">Intermediate</option>
-					<option value="beginner">Beginner</option>
-				</select>
-          	</div>
+				<div className="wmca-form wdgt">
+					<label for="dropdown" class="wmca-form-label">Skill level</label>
+					<select className="" onChange={this.handleChange}>
+						<option value="">Select</option>
+						<option value="advanced">Advanced</option>
+						<option value="intermediate">Intermediate</option>
+						<option value="beginner">Beginner</option>
+					</select>
+				</div>
 			</>
 		)
 	}
 
 	render() {
 		return (
-		<div>
-			{this.SkillLevelFunction()}
-		</div>
+			<div>
+				{this.SkillLevelFunction()}
+			</div>
 		)
 	}
 
