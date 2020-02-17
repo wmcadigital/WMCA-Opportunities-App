80a409ba087906c538e6cdfc3a79880b0ab8ec88
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-17 14:13:18 +0000

    updated files, working on PostCode and Age

--- src/components/sidebar/postcode.jsx
+++ src/components/sidebar/postcode.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 
 class Postcode extends Component {
-    constructor(props) {
-      super(props)
-      this.selectedLevel = "";
-    
-    }
-
-    render() {
-      return (
-        <div className="wmca-form wdgt">
-          <label htmlFor="radio" className="wmca-form-label">
-            Postcode - e.g. DY7 4PU
-            <input className="pure-u-1-1" type="search" name="searchStories">
-            </input>
-          </label>
-        </div>
-      )
-    }
+  state = {
+    term: ''
   }
+  
+
+  // handlePostCodeCheck(event){
+  //   this.setState({
+  //     input: event.target.value
+  //   })
+  //   console.log(event.target.value)
+  // }
+
+  render() {
+    return (
+      <div className="wmca-form wdgt">
+        <label htmlFor="radio" className="wmca-form-label">
+          Postcode - e.g. DY7 4PU
+            <input
+            className="pure-u-1-1"
+            type="text"
+            name="searchStories"
+            value={this.state.term}
+            onChange={e => this.setState({ term: e.target.value.toUpperCase() })}
+            />
+            </label>
+      </div>
+    )
+  }
+}
 
 export default Postcode;
