e85fab22d25618c8b282f8133063277938c37d72
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-02-17 10:16:09 +0000

    Filter for Eligibilty is now fixed

--- src/components/sidebar/eligibility.jsx
+++ src/components/sidebar/eligibility.jsx
@@ -1,75 +1,117 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 
-class Eligibility extends Component {
+class eligibility extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            match: this.props.match
-        };
+  constructor(props) {
+    super(props);
 
-        this.radio_eachEligibility = this.radio_eachEligibility.bind(this);
-    }
+    this.radio_allEligibility = this.radio_allEligibility.bind(this);
+    this.radio_eachEligibility = this.radio_eachEligibility.bind(this); 
+  }
 
-    radio_eachEligibility = () => {
 
-        const { eligibilities, match } = this.props;
-        const eligibilitiesFlatted = eligibilities && eligibilities.flat();
-        const eligibilitiesNewSet = new Set(eligibilitiesFlatted);
-        const eligibilitiesArray = [...eligibilitiesNewSet];
+  //All Eligibility Opportunities
+  radio_allEligibility = () => {
 
+    const { match } = this.props;
+
+    return (
+      <div key="All-Eligibility">
+        <Link to="/opportunities/">
+          <span className="wmca-form__radio">
+            <label htmlFor="All Eligibility" className="wmca-form__radio-label">
+              All Eligibility
+		            </label>
+            {match.url === "/opportunities/" && (
+              <input
+                type="radio"
+                name="All-Eligibility"
+                id="All-Eligibility"
+                value="All Eligibility"
+                defaultChecked
+              />
+            )}
+            {match.url !== "/opportunities/" && (
+              <input
+                type="radio"
+                name="All-Eligibility"
+                id="All-Eligibility"
+                value="All Eligibility"
+              />
+            )}
+            <span className="wmca-form__radio-checkmark"> </span>
+          </span>
+        </Link>
+      </div>
+    )
+  }
+
+  //Each Eligibility Opportunies Buttons
+  radio_eachEligibility = () => {
+
+    const { eligibilities, match } = this.props;
+    const eligibilitiesFlatted = eligibilities && eligibilities.flat();
+    const eligibilitiesNewSet = new Set(eligibilitiesFlatted);
+    const eligibilitiesArray = [...eligibilitiesNewSet];
+
+    return (
+      eligibilities &&
+      eligibilitiesArray.map(eligibility => {
+        const encodedUrl = encodeURIComponent(eligibility)
+          .toLowerCase()
+          .replace(/'/g, escape);
+        const decodedUrl = decodeURIComponent(encodedUrl);
         return (
-            eligibilities &&
-            eligibilitiesArray.map(eligibility => {
-            const encodedUrl = encodeURIComponent(eligibility)
-              .toLowerCase()
-              .replace(/'/g, escape);
-            const decodedUrl = decodeURIComponent(encodedUrl);
-            return (
-              <div key={eligibility}>
-                <Link to={`/opportunities/eligibilities/${encodedUrl}`}>
-                  <span className="wmca-form__radio">
-                    <label htmlFor={eligibility} className="wmca-form__radio-label">
-                      {eligibility}
-                    </label>
-                    {match.params.eligibility === decodedUrl && (
-                        <input
-                        type="radio"
-                        name="radio"
-                        id={eligibility}
-                        value={eligibility}
-                        checked={this.state.selectedEligibility === 'option1'}
-                      />
-                    )}
-                    {match.params.eligibility !== decodedUrl && (
-                          <input 
-                          type="radio" 
-                          name="radio" 
-                          id={eligibility} 
-                          value={eligibility}
-                          />
-                    )}
-                    <span className="wmca-form__radio-checkmark"> </span>
-                  </span>
-                </Link>
-              </div>
-           )
-          })
-        );
-    }
-    
-    render() {
-        return (
-            <>
-            <label htmlFor="radio" className="wmca-form-label">
-                Are you currently employed?
-            </label>
-            {this.radio_eachEligibility()}
-            </>
-        );
-    }
+          <div key={eligibility}>
+            <Link to={`/opportunities/eligibilities/${encodedUrl}`}>
+              <span className="wmca-form__radio">
+                <label htmlFor={eligibility} className="wmca-form__radio-label">
+                  {eligibility}
+                </label>
+                {match.params.eligibility === decodedUrl && (
+                  <input
+                    type="radio"
+                    name="radio"
+                    id={eligibility}
+                    value={eligibility}
+                    defaultChecked
+                  />
+                )}
+                {match.params.eligibility !== decodedUrl && (
+                  <input
+                    type="radio"
+                    name="radio"
+                    id={eligibility}
+                    value={eligibility}
+                  />
+                )}
+                <span className="wmca-form__radio-checkmark"> </span>
+              </span>
+            </Link>
+          </div>
+        )
+      })
+    );
+  }
 
+
+
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="radio" className="wmca-form-label">
+          Are you currently employed?
+        </label>
+
+        <div className="wmca-form__radio" id="radio">
+          {this.radio_allEligibility()}
+          {this.radio_eachEligibility()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Eligibility;
+export default eligibility
---
+++ src/components/sidebar/eligibilityOLD.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Eligibility extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            match: this.props.match
+        };
+
+        this.radio_eachEligibility = this.radio_eachEligibility.bind(this);
+    }
+
+    radio_eachEligibility = () => {
+
+        const { eligibilities, match } = this.props;
+        const eligibilitiesFlatted = eligibilities && eligibilities.flat();
+        const eligibilitiesNewSet = new Set(eligibilitiesFlatted);
+        const eligibilitiesArray = [...eligibilitiesNewSet];
+
+        return (
+            eligibilities &&
+            eligibilitiesArray.map(eligibility => {
+                const encodedUrl = encodeURIComponent(eligibility)
+                    .toLowerCase()
+                    .replace(/'/g, escape);
+                const decodedUrl = decodeURIComponent(encodedUrl);
+                return (
+                    <div key={eligibility}>
+                        <Link to={`/opportunities/eligibilities/${encodedUrl}`}>
+                            <span className="wmca-form__radio">
+                                <label htmlFor={eligibility} className="wmca-form__radio-label">
+                                    {eligibility}
+                                </label>
+                                {match.params.eligibility === decodedUrl && (
+                                    <input
+                                        type="radio"
+                                        name="radio"
+                                        id={eligibility}
+                                        value={eligibility}
+                                        checked={this.state.selectedEligibility === 'option1'}
+                                    />
+                                )}
+                                {match.params.eligibility !== decodedUrl && (
+                                    <input
+                                        type="radio"
+                                        name="radio"
+                                        id={eligibility}
+                                        value={eligibility}
+                                    />
+                                )}
+                                <span className="wmca-form__radio-checkmark"> </span>
+                            </span>
+                        </Link>
+                    </div>
+                )
+            })
+        );
+    }
+
+    render() {
+        return (
+            <>
+                <label htmlFor="radio" className="wmca-form-label">
+                    Are you currently employed?
+            </label>
+                {this.radio_eachEligibility()}
+            </>
+        );
+    }
+
+}
+
+export default Eligibility;
